c406c389a6b054b0a1767b682af2fcb83674974b
TMaj <tahamajlesi@ut.ac.ir>
2023-08-09 23:48:57 +0330

    delete some files

--- build.sh
+++
@@ -1,1 +0,0 @@
- g++ source/main.cpp source/LevelScreen.cpp source/menu.cpp source/AudioClip.cpp source/star.cpp source/MusicPlayer.cpp source/map.cpp source/TurtleChile.cpp source/Game.cpp source/animation.cpp source/GamePause.cpp source/Player.cpp source/Score.cpp source/Enemy.cpp  -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio ; ./a.out
